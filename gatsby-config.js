8185c8e94b0873eb8cbd93c14d1668f6d8998b2d
Dwight Doane <dwightdoane@me.com>
2019-08-23 20:37:02 -0400

    add analytics id

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-97080570-1`,
       },
     },
     `gatsby-plugin-feed`,
